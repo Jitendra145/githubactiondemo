30aff378e6d683f41fe6c104e9dd3d868463658b
jitenverma145@gmail.com <jitenverma145@gmail.com>
2026-08-28 22:47:41 +0530

    make maven wrapper executable

--- src/main/java/com/jv/githubactiondemo/GithubactiondemoApplication.java
+++ src/main/java/com/jv/githubactiondemo/GithubactiondemoApplication.java
@@ -8,6 +8,7 @@ public class GithubactiondemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactiondemoApplication.class, args);
+        System.out.println("test");
 	}
 
 }
